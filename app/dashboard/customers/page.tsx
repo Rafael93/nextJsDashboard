417185c55c66b2654cc0cd40f5d0363b44a65693
Rafael93 <rafaelgamas@gmail.com>
2024-07-12 13:04:18 -0400

    "initial structure"

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const customers = () => {
+  return <h1>Customers page</h1>;
+};
+
+export default customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const invoices = () => {
+  return <h1>Invoices Page</h1>;
+};
+
+export default invoices;
